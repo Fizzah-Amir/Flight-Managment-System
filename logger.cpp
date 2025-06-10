dc097932b4102e5ea71c6d4d56772a15dcfd52cf
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-10 18:50:30 +0500

    logger.cpp

---
+++ logger.cpp
@@ -0,0 +1,26 @@
+#include <ctime>
+#include"Logger.h"
+#include <iomanip>
+Logger::Logger() {};
+Logger& Logger::getInstance() {
+    static Logger instance;
+    return instance;
+}
+void Logger::initialize(const char* filename) {
+    file.open(filename, ios::app);
+    if (!file.is_open()) {
+        cout << "could not open";
+    }
+}
+void Logger::cleanup() {
+    if (file.is_open()) {}
+    file.close();
+}
+void Logger::log(const char* msg) {
+    if (file.is_open()) {
+      time_t now = time(0);
+      tm* tm_now = localtime(&now);
+      file << put_time(tm_now, "%Y-%m-%d %H:%M:%S") << msg;
+      cout << endl;
+    }
+}
