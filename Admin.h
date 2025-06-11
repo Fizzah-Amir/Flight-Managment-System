128e853c3967172fe8f36d3eed966bae00d0d6d1
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-11 23:13:17 +0500

    Add files via upload

---
+++ Admin.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "Person.h"
+#include"Mystring.h"
+class Admin : public Person {
+public:
+ Admin();
+ Admin(Mystring n,  Address a, Mystring e,  Mystring p);
+  void addFlight();
+  void cancelFlight();
+  void sendNoti(Mystring msg);
+};
