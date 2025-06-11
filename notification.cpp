1a7b055d2d8d5a6d52e4cd4f9a3b1f8e098bfbf5
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-11 15:26:08 +0500

    notification.cpp

---
+++ notification.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+#include"Notification.h"
+Notification::Notification() : notiId(0), msg("") {}
+Notification::Notification(int id,  Mystring m) : notiId(id),msg(m) {}
+ void Notification:: send() { 
+	Logger::getInstance().log(msg);
+}
