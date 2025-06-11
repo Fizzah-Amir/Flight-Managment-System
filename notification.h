8fca1a027ca32e280a70310f65a170c61e94e5db
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-11 15:25:36 +0500

    notification.h

---
+++ notification.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Mystring.h"
+#include"Logger.h"
+class Notification {
+protected:
+   int notiId;
+    Mystring msg;
+public:
+    Notification();
+    Notification(int id, Mystring m);
+    virtual void send();
+};
