04a74811aa4b489c0958a7eb10310ee59e08bbc5
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-11 23:15:02 +0500

    Add files via upload

---
+++ flight_inst.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "Mystring.h"
+class FlightInstance {
+    int instanceId;
+    Mystring date;
+    bool cancel;
+    int reserve_seat;
+public:
+   FlightInstance(int iid, Mystring d) : instanceId(iid), date(d), cancel(false), reserve_seat(0) {}
+   int getInstanceID();
+   Mystring getDate();
+   bool isCancel();
+   void cancelled();
+   void reserveSeat();
+   void unreserveSeat();
+   int getReservedSeats();
+   void print();
+   FlightInstance(const FlightInstance& other);
+       
+
+};
