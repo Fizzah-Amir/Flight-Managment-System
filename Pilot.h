f61afd1ccd226a2721b0ad22b5223940f69042ef
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-12 18:57:21 +0500

    Pilot.h

---
+++ Pilot.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Person.h"
+#include"logger.h"
+class Pilot : public Person {
+    int assignedFlight[5];
+    int assignCount;
+public:
+    Pilot();
+    Pilot( Mystring n, Address a, Mystring e,Mystring p);
+    void assignFlight(int flightID);
+    void printAssignedFlights();
+};
