2695b58b4bbd920d9895a3f56040c508831796ef
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-12 05:47:29 +0500

    Flight.h

--- Flight.h
+++ Flight.h
@@ -1,13 +1,30 @@
 #pragma once
-#ifndef FLIGHT_H
-#define FLIGHT_H
-class Flight{
-int flightID;
-Mystring flightNo;
-Mystring departureCity;
-Mystring arrivalCity;
-int duration;
-Myvector<WeeklySchedule> schedules;
-Myvector<FlightInstance> instances;
-}
-#endif
+#include "Myvector.h"
+#include "Mystring.h"
+#include"flight_inst.h"
+#include "schedule.h"
+#include "Logger.h"
+#include "Reservation.h"
+class Flight {
+    int flightId;
+    Mystring flightNumber;
+    Mystring from;
+       Mystring to;
+    int duration;
+    MyVector<Schedule> schedules;
+  MyVector<FlightInstance> instances;
+public:
+    Flight(int id,Mystring num, Mystring dep,  Mystring arr, int dur) : flightId(id), flightNumber(num), from(dep), to(arr), duration(dur) {};
+    Mystring getFlightNumber();
+      int getFlightId();
+      MyVector<FlightInstance>& get_instances();
+      void addSchedule(const Schedule& s);
+       void addInstance(const FlightInstance& fi);
+      void print()const;
+      Mystring get_from_city()const;
+      Mystring get_to_city()const;
+      static void show_schedule(const MyVector<Flight>& flights, MyVector<Reservation>& reservations);
+       static void bookTicket(MyVector<Flight>&flights, MyVector<Reservation>&reservations);
+       static void searchFlights(MyVector<Flight>&flights);
+        static void update_schedule(MyVector<Flight>&flights);
+};
