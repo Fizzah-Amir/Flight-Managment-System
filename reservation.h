39deabf7db8479990e51d0c6fa31f9b7105b5d1d
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-12 08:23:04 +0500

    reservation.h

---
+++ reservation.h
@@ -0,0 +1,21 @@
+#pragma once
+#include"Myvector.h"
+#include "Mystring.h"
+#include "Logger.h"
+class Flight;
+class Reservation {
+    int r_Id;
+    int flightId;
+    int instanceId;
+    Mystring passen_name;
+public:
+    Reservation();
+    Reservation(int rid, int fid, int iid, Mystring pname);
+    int get_reservation_Id();
+    int getFlightId();
+    int getInstanceId();
+    Mystring getpassen_name();
+    void print();
+    static void Modify_Cancel_Reservation(MyVector<Flight>flights, MyVector<Reservation>reservations);
+};
+        
