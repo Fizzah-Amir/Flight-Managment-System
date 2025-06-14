d0cb4eb480d031207d2d0a7e04823b5842a45304
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-14 21:47:32 +0500

    Pilot.h

--- Pilot.h
+++ Pilot.h
@@ -2,8 +2,8 @@
 #include "Person.h"
 #include"logger.h"
 class Pilot : public Person {
-    int assignedFlight[5];
-    int assignCount;
+  int assignedFlight[5];
+ int assignCount;
 public:
     Pilot();
     Pilot( Mystring n, Address a, Mystring e,Mystring p);
