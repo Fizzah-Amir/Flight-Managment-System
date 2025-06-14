e40957cd5f0f2ac74c71a8c02a013882431e9f40
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-14 21:31:43 +0500

    sms_notification.h

--- sms_notification.h
+++ sms_notification.h
@@ -3,10 +3,10 @@
 #include"Logger.h"
 #include"Mystring.h"
 class SmsNotification : public Notification {
-    Mystring phone;
+Mystring phone;
 public:
-    SmsNotification();
-    SmsNotification(int id,const Mystring& c, const Mystring& ph);
-    void setPhone(const Mystring& ph);
-    void send()override;
+SmsNotification();
+SmsNotification(int id,const Mystring& c, const Mystring& ph);
+void setPhone(const Mystring& p);
+void send()override;
 };
