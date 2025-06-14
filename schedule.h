7ca0dbb2c127c5045684d11a8287629b2dd45cb7
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-14 21:35:06 +0500

    schedule.h

--- schedule.h
+++ schedule.h
@@ -4,11 +4,11 @@ class Schedule {
     int day;
     Mystring depart_time;
 public:
-    Schedule() : day(0), depart_time("00:00") {}
-   Schedule(int day, Mystring t) : day(day), depart_time(t) {}
-    void setDay(int d);
-    void setDepartureTime(Mystring t);
-    int getDay();
-    Mystring getDepartureTime();
+Schedule() : day(0), depart_time("12:00") {}
+Schedule(int day, Mystring t) : day(day), depart_time(t) {}
+void setDay(int d);
+ void setDepartureTime(const Mystring &t);
+int getDay();
+Mystring get_departure_time();
 };
 
