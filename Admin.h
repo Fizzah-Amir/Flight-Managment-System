c3843a25a0b840493c8a2f4a1573d7529b7dc856
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-14 21:52:51 +0500

    Admin.h

--- Admin.h
+++ Admin.h
@@ -4,7 +4,7 @@
 class Admin : public Person {
 public:
  Admin();
- Admin(Mystring n,  Address a, Mystring e,  Mystring p);
+Admin(Mystring n,  Address a, Mystring e,  Mystring p);
   void addFlight();
   void cancelFlight();
   void sendNoti(Mystring msg);
