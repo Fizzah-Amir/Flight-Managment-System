6a4e1fb82fc64f2a12d55e7dc5101dd6abd782fc
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-14 21:41:02 +0500

    notification.cpp

--- notification.cpp
+++ notification.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
 using namespace std;
 #include"Notification.h"
-Notification::Notification() : notiId(0), msg("") {}
-Notification::Notification(int id,  Mystring m) : notiId(id),msg(m) {}
+Notification::Notification() : notiId(0), msg("") {};
+Notification::Notification(int id,  Mystring m) : notiId(id),msg(m) {};
  void Notification:: send() { 
-	Logger::getInstance().log(msg);
+   Logger::getInstance().log(msg);
 }
