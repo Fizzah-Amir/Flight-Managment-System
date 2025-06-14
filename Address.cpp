3663665715705f7dad5f3acdbb8c1d8dcdc04d00
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-14 21:54:21 +0500

    Update Address.cpp/

--- Address.cpp
+++ Address.cpp
@@ -5,20 +5,20 @@ Address::Address() : city(""), state(""), country(""){}
 void Address::setCity(const Mystring& c) 
 { 
 	city = c; 
-}
+   }
 void Address::setState(const Mystring& s) { 
 	state = s;
-}
+  }
 void Address::setCountry(const Mystring& c) {
 	country = c;
-}
+   } 
 
 Mystring Address::getCity() {
 	return city;
-}
+   }
 Mystring Address::getState() { 
 	return state;
-}
+  }
 Mystring Address:: getCountry(){ 
 	return country;
 }
