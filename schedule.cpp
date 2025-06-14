4b4d1f7e959038ba24b4411d617dd093d05096a4
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-14 21:35:59 +0500

    schedule.cpp

--- schedule.cpp
+++ schedule.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 #include"schedule.h"
-Schedule::Schedule() : day(0), depart_time("00:00") {}
+Schedule::Schedule() : day(0), depart_time("12:00") {}
 Schedule::Schedule(int day,  Mystring t) : day(day), depart_time(t) {}
 void Schedule::setDay(int d) { 
         day= d; 
@@ -9,9 +9,9 @@ void Schedule::setDay(int d) {
 void Schedule::setDepartureTime(Mystring t) {
         depart_time = t;
     }
- int Schedule::getDay(){
-        return day;
+int Schedule::getDay(){
+   return day;
     }
-  Mystring Schedule::getDepartureTime() {
-        return depart_time;
+Mystring Schedule::get_departure_time() {
+return depart_time;
     }
