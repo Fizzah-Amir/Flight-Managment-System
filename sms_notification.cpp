802d2f6dce3767fc9adc3f8a66a0831d64a62364
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-14 21:33:06 +0500

    sms_notification.cpp

--- sms_notification.cpp
+++ sms_notification.cpp
@@ -1,12 +1,11 @@
 #include<iostream>
 using namespace std;
 #include"sms_notification.h"
-SmsNotification::SmsNotification() : Notification(), phone("p") {}
-SmsNotification::SmsNotification(int id, const Mystring& c, const Mystring& ph) : Notification(id, c), phone(ph) {}
-void SmsNotification::setPhone(const Mystring& ph) {
-	phone = ph;
+SmsNotification::SmsNotification() : Notification(), phone("0") {}
+SmsNotification::SmsNotification(int id, const Mystring& c, const Mystring& ph):Notification(id, c),phone(ph){};
+void SmsNotification::setPhone(const Mystring& p) {
+        phone = p;
  }
 void SmsNotification::send() { 
-
-   Logger::getInstance().log("SMS sent."); 
+   Logger::getInstance().log("sms sent."); 
 }
