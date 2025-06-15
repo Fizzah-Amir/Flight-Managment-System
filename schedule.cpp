50bbbeeff66f8991a752f6805bf58a095be69a91
Fizzah-Amir <bscs24031@itu.edu.pk>
2025-06-15 12:15:02 +0500

    schedule.cpp

--- schedule.cpp
+++ schedule.cpp
@@ -15,3 +15,22 @@ int Schedule::getDay(){
 Mystring Schedule::get_departure_time() {
 return depart_time;
     }
+ void Schedule::save(fstream& f) {
+     
+     int l = depart_time.length();
+     f.write((char*)&l, sizeof(l));
+     f.write((const char*)depart_time, l);
+
+}
+ void Schedule::load(fstream& f) {
+         
+       int l;
+      f.read((char*)&l, sizeof(l));
+        char* temp = new char[l + 1];
+        f.read(temp, l);
+         temp[l] = '\0';
+         delete[] temp;
+     }
+
+
+
